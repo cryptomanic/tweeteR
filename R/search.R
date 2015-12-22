646ef29155ec5e9c5c739392556f6b41335ac0ed
cryptomanic <deepakyadav.iitbhu@gmail.com>
2015-12-22 23:10:35 +0530

    add userid parameter to userInfo

--- R/search.R
+++ R/search.R
@@ -15,26 +15,28 @@
 #' hashTag(token, "oldisgold", count = 50, lang = "zh")
 #' @export
 
-hashTag <- function(token = NULL, hashtag = NULL, count = 15, lang = "en") {
-  if (is.null(token) || is.null(hashtag)) {
-    stop('Neither token nor hashtag can be NULL')
+hashTag <-
+  function(token = NULL, hashtag = NULL, count = 15, lang = "en") {
+    if (is.null(token) || is.null(hashtag)) {
+      stop('Neither token nor hashtag can be NULL')
+    }
+
+    url <- "https://api.twitter.com/1.1/search/tweets.json?q=%23"
+    url <- paste0(url, hashtag, "&count=", count, "&lang=", lang)
+
+    ldf <- APIcall(token, url)
+    ldf <- ldf$statuses
+
+    tweets    <- gsub(x = ldf$text, pattern = "https?://.+$|\\n","")
+    ldf <- ldf$user
+
+    user_id     <-  sapply(ldf$id, function(x) x)
+    user_name   <-  sapply(ldf$name, function(x) x)
+    location    <-  sapply(ldf$location, function(x) x)
+    img_profile <<- sapply(ldf$profile_image_url, function(x) x)
+
+    data.frame(user_id, user_name, location, tweets)
   }
-
-  url <- "https://api.twitter.com/1.1/search/tweets.json?q=%23"
-  url <- paste0(url, hashtag, "&count=", count, "&lang=", lang)
-
-  ldf <- APIcall(token, url)
-  ldf <- ldf$statuses
-
-  tweets    <- gsub(x = ldf$text, pattern = "https?://.+$|\\n","")
-  ldf <- ldf$user
-  user_id     <-  sapply(ldf$id, function(x) x)
-  user_name   <-  sapply(ldf$name, function(x) x)
-  location    <-  sapply(ldf$location, function(x) x)
-  img_profile <<- sapply(ldf$profile_image_url, function(x) x)
-
-  data.frame(user_id, user_name, location, tweets)
-}
 
 #' User Information.
 #'
@@ -42,26 +44,38 @@ hashTag <- function(token = NULL, hashtag = NULL, count = 15, lang = "en") {
 #'
 #' @param token Access Token
 #' @param user_name Screen Name
+#' @param user_id User Id
 #'
 #' @examples
 #' userInfo(token, "put username here")
 #' @export
 
-userInfo <- function(token = NULL, user_name = NULL) {
-  if (is.null(token) || is.null(user_name)) {
-    stop('Neither token nor user name can be NULL')
+userInfo <-
+  function(token = NULL, user_name = NULL, user_id = NULL) {
+    if (is.null(token)) {
+      stop("Token cannot be null")
+    }
+    if (is.null(user_name) && is.null(user_id)) {
+      stop("username and userid cannot be null simultaneously")
+    }
+
+    url <-
+      "https://api.twitter.com/1.1/statuses/user_timeline.json?count=1&"
+    if (is.null(user_id)) {
+      url <- paste0(url, "screen_name=", user_name)
+    } else {
+      url <- paste0(url, "id=", user_id)
+    }
+
+    ldf <- APIcall(token, url)
+    ldf <- ldf$user
+    profile_pic <<- ldf$profile_image_url[[1]]
+
+    list(
+      id          = ldf$id[[1]],
+      name        = ldf$name[[1]],
+      location    = ldf$location[[1]],
+      description = ldf$description[[1]],
+      followers   = ldf$followers_count[[1]]
+    )
   }
-
-  url <- "https://api.twitter.com/1.1/statuses/user_timeline.json?"
-  url <- paste0(url, "count=1&screen_name=", user_name)
-
-  ldf <- APIcall(token, url)
-  ldf <- ldf$user
-  profile_pic <<- ldf$profile_image_url[[1]]
-
-  list(id          = ldf$id[[1]],
-       name        = ldf$name[[1]],
-       location    = ldf$location[[1]],
-       description = ldf$description[[1]],
-       followers   = ldf$followers_count[[1]])
-}
